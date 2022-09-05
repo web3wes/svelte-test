4d812024806b9278dcd14b73633b2670b7be554f
Wes Jones <wes@Wess-MacBook-Pro-4.local>
2022-09-05 01:38:22 -0400

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	// preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '200.html'
+		}),
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'continue',
+			entries: ['*'],
+
+		}
 	}
 };
 
